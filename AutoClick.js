cce4472384bd05b7147e2245ed64bc35acfdc917
WisnuCakraa <you@example.com>
2021-01-30 07:57:27 +0700

    autoclick.js

---
+++ AutoClick.js
@@ -0,0 +1,29 @@
+setInterval(() => {
+  $disabled = false;
+  if (!$disabled) {
+    let btn = document.getElementsByTagName("button");
+    for (let i = 0; i < btn.length; i++) {
+      if (btn[i].innerHTML.includes("Like")) {
+        btn[i].click();
+        break;
+      }
+    }
+  }
+}, 2000);
+
+function likeFunction() {
+  let wisnuBlog = document.querySelector(".coreSpriteRightPaginationArrow");
+  likeCount++;
+  console.log("Wisnu Like " + likeCount);
+  var wisnuKeren = Math.random() * (5000 - 5000) + 4000;
+  setTimeout(function () {
+    wisnuBlog.click();
+  }, 1850);
+  if (likeCount < 25) {
+    setTimeout(likeFunction, wisnuKeren);
+  } else {
+    console.log("Nice! jagoan kode emang joss.");
+  }
+}
+var likeCount = 0;
+likeFunction();
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ex01-autoclick-instagram
+
+## Usage
+
+jika ingin menambah jumblah count like ganti code berikut :
+
+`if (likeCount < 25)`ganti angka 26 itu menjadi angka yang diinginkan
